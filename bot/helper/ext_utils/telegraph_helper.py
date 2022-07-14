9ca9d88517ef48dd62547e98bcc96296ec8272d8
vivekdarji00 <70027857+vivekdarji00@users.noreply.github.com>
2022-07-14 17:32:59 +0530

    Update telegraph_helper.py

--- bot/helper/ext_utils/telegraph_helper.py
+++ bot/helper/ext_utils/telegraph_helper.py
@@ -72,10 +72,10 @@ class TelegraphHelper:
                     nxt_page += 1
             self.edit_page(
                 path = path[prev_page],
-                title = 'Mirror-leech-bot Torrent Search',
+                title = 'ToonsWood.In',
                 content=content
             )
         return
 
 
-telegraph=TelegraphHelper('Mirror-Leech-Telegram-Bot', 'https://github.com/anasty17/mirror-leech-telegram-bot')
+telegraph=TelegraphHelper('ToonsWood.In', 'https://www.toonswood.in')
